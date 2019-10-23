63167ef11b120507bd35bc2e3badd30cbb25d7b6
Nick <30442112+MrSrNick@users.noreply.github.com>
2019-10-23 11:26:29 -0500

    adding port

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.post('/imageurl', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('asdf');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
 /*
